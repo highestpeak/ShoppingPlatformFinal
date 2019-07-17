c587694df103b6a7e01b2e6aabf0e99b10959426
Karl Liu <karl.liu.1024@gmail.com>
2019-07-17 14:42:42 +0800

    add commodity and shopping cart model; add shopping cart service;

---
+++ src/main/java/com/demo/mms/common/domain/ShoppingCart.java
@@ -0,0 +1,22 @@
+package com.demo.mms.common.domain;
+
+public class ShoppingCart {
+    private Integer id;
+    private Integer commodity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCommodity() {
+        return commodity;
+    }
+
+    public void setCommodity(Integer commodity) {
+        this.commodity = commodity;
+    }
+}
