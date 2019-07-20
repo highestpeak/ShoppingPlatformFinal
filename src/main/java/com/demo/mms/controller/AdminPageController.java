c8c69838d17fd211ed6daf6c0ad9aa88fbe780af
cheapTalk <highestpeak@163.com>
2019-07-20 15:52:08 +0800

    后台页面连接

--- src/main/java/com/demo/mms/controller/AdminPageController.java
+++ src/main/java/com/demo/mms/controller/AdminPageController.java
@@ -8,11 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminPageController {
     @RequestMapping("/")
     public String toAdminHome(){
-        return "admin_userManage";
+        return "admin_home";
+    }
+
+    @RequestMapping("/classifyManage")
+    public String toClassifyManage(){
+        return "admin_classifyManage";
+    }
+
+    @RequestMapping("/goodsManage")
+    public String toGoodsManage(){
+        return "admin_goodManage";
+    }
+
+    @RequestMapping("/orderManage")
+    public String toOrderManage(){
+        return "admin_orderManage";
     }
 
     @RequestMapping("/userManage")
     public String toUserManage(){
         return "admin_userManage";
     }
+
+    @RequestMapping("/storeInfoManage")
+    public String toStoreInfoManage(){
+        return "admin_storeInfoManage";
+    }
+
+
 }
---
+++ web/WEB-INF/page/admin_storeInfoManage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 自由在风中
+  Date: 2019/7/20
+  Time: 15:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>admin_storeInfoManage</title>
+</head>
+<body>
+
+</body>
+</html>
