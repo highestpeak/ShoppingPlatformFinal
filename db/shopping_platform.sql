3b931a4f98376d8eaaed1036a0e6e6fadf16ae66
cheapTalk <highestpeak@163.com>
2019-07-23 10:31:10 +0800

    新数据库构建文件

--- db/shopping_platform.sql
+++ db/shopping_platform.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80015
 File Encoding         : 65001
 
-Date: 2019-07-22 15:02:56
+Date: 2019-07-23 10:30:47
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -72,6 +72,11 @@ CREATE TABLE `classify_of_goods` (
 -- ----------------------------
 INSERT INTO `classify_of_goods` VALUES ('2', '2', '003');
 INSERT INTO `classify_of_goods` VALUES ('3', '3', '101');
+INSERT INTO `classify_of_goods` VALUES ('4', '4955cf9b1909412cbfd8479a8162a0b7', '001');
+INSERT INTO `classify_of_goods` VALUES ('5', 'a3fc77b3d94845c68f726aa6b2a42e52', '002');
+INSERT INTO `classify_of_goods` VALUES ('5818e501a638416abb9c0448864aadc4', 'a3fc77b3d94845c68f726aa6b2a42e52', '108e7176cd0e4a1dbfa8c73263994307');
+INSERT INTO `classify_of_goods` VALUES ('61eb48d1934a475b8221310af3321b86', 'a3fc77b3d94845c68f726aa6b2a42e52', 'f3e60646e899474990f4bb5a0fc2ef26');
+INSERT INTO `classify_of_goods` VALUES ('9e9f0dd75c344d25bf89ccebda72da50', 'a3fc77b3d94845c68f726aa6b2a42e52', '59181ea38f0742a191964843b04ddea8');
 
 -- ----------------------------
 -- Table structure for goods
@@ -97,10 +102,24 @@ CREATE TABLE `goods` (
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
+INSERT INTO `goods` VALUES ('001', '0000', '快乐的快乐水', '0', '第一个商店的商品', null, null, null, '20190722-215855', '20190722-215855');
+INSERT INTO `goods` VALUES ('002', '0000', '不快乐的水', '0', '阿斯顿撒', null, null, null, '20190722-215855', '20190722-215855');
 INSERT INTO `goods` VALUES ('003', '0000', 'github文化衫', '0', '修改描述测试', 'https://www.google.com/', '', '', '20190718-134040', '20190721-222604');
+INSERT INTO `goods` VALUES ('053e64e8ee0948d7a7f93849feca8911', '0000', 'helloworld的仆人', '1024', '阿三大苏打实打实', null, '卖着呢', 'new', '20190722-224758', '20190722-224758');
 INSERT INTO `goods` VALUES ('101', '0001', '川大文化衫', '0', null, 'https://www.google.com/', 'selling', 'new', '20190718-111541', '20190718-111541');
 INSERT INTO `goods` VALUES ('102', '0001', '川大快乐水', '0', null, 'https://www.google.com/', 'selling', 'new', '20190718-111541', '20190718-111541');
 INSERT INTO `goods` VALUES ('103', '0001', '肥宅的快乐', '0', null, 'https://www.google.com/', 'selling', 'new', '20190718-111541', '20190718-111541');
+INSERT INTO `goods` VALUES ('108e7176cd0e4a1dbfa8c73263994307', '0000', '伟大的码农5', '1024', '站在天台上的码农', null, '即将跳下来', '没头发', '20190723-102625', '20190723-102625');
+INSERT INTO `goods` VALUES ('2b0012cdaa4e43799e7bd443b7b2d91f', '0000', 'helloworld的宠儿', '2048', '阿三大苏打实打实', null, '卖着呢', 'new', '20190722-224923', '20190722-224923');
+INSERT INTO `goods` VALUES ('2e7cc5e5486e455aadf6c746b4136184', '0000', '伟大的码农0', '1024', '站在天台上的码农', null, '即将跳下来', '没头发', '20190722-230817', '20190722-230817');
+INSERT INTO `goods` VALUES ('59181ea38f0742a191964843b04ddea8', '0000', '伟大的码农3', '1024', '站在天台上的码农', null, '即将跳下来', '没头发', '20190722-231835', '20190722-231835');
+INSERT INTO `goods` VALUES ('5a19450b764c4ffc9503d2a2e7e6f7b0', '0000', 'helloworld的爸爸', '2048', '阿三大苏打实打实', null, '卖着呢', 'new', '20190722-225142', '20190722-225142');
+INSERT INTO `goods` VALUES ('835cb022a93e45778b747acbc608acb4', '0000', 'helloworld的妈妈', '2048', '阿三大苏打实打实', null, '卖着呢', 'new', '20190722-225222', '20190722-225222');
+INSERT INTO `goods` VALUES ('835e382d60c44a96be7222438f20ea17', '0000', 'helloworld的姐姐', '2048', '阿三大苏打实打实', null, '卖着呢', 'new', '20190722-225415', '20190722-225415');
+INSERT INTO `goods` VALUES ('bda5075cf4c547d791245c9216a41568', '0000', 'helloworld', '0', '阿三大苏打实打实', null, '卖着呢', 'new', '20190722-162227', '20190722-162227');
+INSERT INTO `goods` VALUES ('dc0b65ad3b6e4f898f4efc3c5d69e40b', '0000', '伟大的码农1', '1024', '站在天台上的码农', null, '即将跳下来', '没头发', '20190722-230857', '20190722-230857');
+INSERT INTO `goods` VALUES ('e105b16a043443f28b0219a850249754', '0000', '伟大的码农', '1024', '站在天台上的码农', null, '即将跳下来', '没头发', '20190722-225639', '20190722-225639');
+INSERT INTO `goods` VALUES ('f3e60646e899474990f4bb5a0fc2ef26', '0000', '伟大的码农4', '1024', '站在天台上的码农', null, '即将跳下来', '没头发', '20190722-231905', '20190722-231905');
 
 -- ----------------------------
 -- Table structure for goods_classify
@@ -119,14 +138,18 @@ CREATE TABLE `goods_classify` (
 -- ----------------------------
 -- Records of goods_classify
 -- ----------------------------
+INSERT INTO `goods_classify` VALUES ('0f12485760664e03a20c93b374e7e3a6', '011', '2273be4ae8a64e23a119c1b1370654e6', 'helloworldchild', '20190722-215118', '20190722-215118');
 INSERT INTO `goods_classify` VALUES ('1', '0', '0', '食品', '20190718-134138', '20190718-134138');
 INSERT INTO `goods_classify` VALUES ('2', '0', '0', '服装', '20190718-111541', '20190718-111541');
+INSERT INTO `goods_classify` VALUES ('2273be4ae8a64e23a119c1b1370654e6', '01', '7', 'helloworld', '20190722-215052', '20190722-215052');
 INSERT INTO `goods_classify` VALUES ('3', '1', '1', '方便面', '20190718-111541', '20190718-111541');
 INSERT INTO `goods_classify` VALUES ('4', '1', '1', '冰红茶', '20190718-111541', '20190718-111541');
 INSERT INTO `goods_classify` VALUES ('5', '2', '3', '汤达人', '20190718-111541', '20190718-111541');
 INSERT INTO `goods_classify` VALUES ('6', '1', '2', '耐克', '20190718-111541', '20190718-111541');
-INSERT INTO `goods_classify` VALUES ('7', '1', '2', '阿迪达斯', '20190718-111541', '20190718-111541');
+INSERT INTO `goods_classify` VALUES ('7', '0', '0', '阿迪', '20190718-111541', '20190718-111541');
 INSERT INTO `goods_classify` VALUES ('8', '2', '6', 'AJ球鞋', '20190718-111541', '20190718-111541');
+INSERT INTO `goods_classify` VALUES ('934049ded75a43f78e80323ccc89c699', '01', '2', '服装的弟弟', '20190722-215217', '20190722-215217');
+INSERT INTO `goods_classify` VALUES ('9be55239a9ce435da119ac8bb61af917', '0', '0', '服装的兄弟', '20190722-215544', '20190722-215544');
 
 -- ----------------------------
 -- Table structure for goods_viewed
@@ -228,7 +251,7 @@ CREATE TABLE `on_sale` (
   `create_time` varchar(19) DEFAULT NULL,
   `due_time` varchar(36) DEFAULT NULL,
   `note` text,
-  `sale_level` varchar(20) DEFAULT NULL,
+  `discount` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`on_sale_id`),
   KEY `on_sale_store_store_id_fk` (`store_id`),
   KEY `on_sale_goods_goods_id_fk` (`goods_id`),
@@ -301,9 +324,12 @@ CREATE TABLE `store_sell_classify` (
 -- ----------------------------
 -- Records of store_sell_classify
 -- ----------------------------
+INSERT INTO `store_sell_classify` VALUES ('4955cf9b1909412cbfd8479a8162a0b7', '0000', '0f12485760664e03a20c93b374e7e3a6');
 INSERT INTO `store_sell_classify` VALUES ('4', '0000', '2');
+INSERT INTO `store_sell_classify` VALUES ('a3fc77b3d94845c68f726aa6b2a42e52', '0000', '2273be4ae8a64e23a119c1b1370654e6');
 INSERT INTO `store_sell_classify` VALUES ('2', '0000', '4');
-INSERT INTO `store_sell_classify` VALUES ('6', '0000', '7');
+INSERT INTO `store_sell_classify` VALUES ('99d73b3df3004ef4801e697ad22a6a1a', '0000', '934049ded75a43f78e80323ccc89c699');
+INSERT INTO `store_sell_classify` VALUES ('698a983495284646b698793d7f212c3c', '0000', '9be55239a9ce435da119ac8bb61af917');
 INSERT INTO `store_sell_classify` VALUES ('3', '0001', '2');
 INSERT INTO `store_sell_classify` VALUES ('7', '0001', '6');
 
